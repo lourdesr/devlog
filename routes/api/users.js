2108f4e66b6563043145052d496f9aa938e96456
Lourdes Roashan <root.roashan@gmail.com>
2018-07-28 17:49:15 -0300

    Minor Route API changes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+const AUTHSECRET = require("../../config/keys").authSecret;
+
 const router = express.Router();
 
 //Load User Model
@@ -55,5 +59,56 @@ router.post("/register", (req, res) => {
     }
   });
 });
+
+//@route    GET/api/users/login
+//@desc     Login Users
+//@access   Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+    }
+
+    bcrypt.compare(password, user.password).then(isMatch => {
+      //returns a bool values in isMatch
+      if (isMatch) {
+        //User matched
+        //res.json({ msg: "Success" });
+        const payload = {
+          id: user.id,
+          name: user.name,
+          avatar: user.avatar
+        };
+
+        jwt.sign(payload, AUTHSECRET, { expiresIn: 3600 }, (err, token) => {
+          //
+          res.json({
+            success: true,
+            token: "Bearer " + token
+          });
+        });
+      } else {
+        //Incorrect Password
+        return res.status(400).json({ password: "Invalid Login Credentials" });
+      }
+    });
+  });
+});
+
+//@route    GET/api/users/current
+//@desc     Return Current Users
+//@access   Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({ msg: "Success" });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -21,14 +22,17 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(() => console.log("Error Connecting to MongoDB"));
 
+//Passport Middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 //Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
-
-app.get("/", (req, res) => res.json("Hello"));
 
 app.listen(port, () => {
   console.log(`Server Running on port ${port}`);
