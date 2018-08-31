8cbaa2eaee4014bf8c3cda87d2b57e8b68f99af6
Lourdes Roashan <root.roashan@gmail.com>
2018-08-31 15:59:52 -0300

    Profile Route Updates

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Load Profile Model
+const Profile = require('../../models/Profile');
+//Load User Profile
+const User = require('../../models/User');
+
 
 router.get("/test", (req, res) => {
   res.json({
     msg: "Profile Works"
   });
 });
+
+
+// @route GET api/profile
+// @desc Get Current User Profile
+// @access Private
+
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const errors = {};
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
