f0dfe27344f4d641891529d024c6e326eb915708
Lourdes Roashan <root.roashan@gmail.com>
2018-09-02 16:29:39 -0300

    Added Post Validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -20,7 +20,7 @@ router.get("/test", (req, res) => {
 // @desc Get Current User Profile
 // @access Private
 
-router.get("/userprofile", passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get("/", passport.authenticate('jwt', { session: false }), (req, res) => {
 
   Profile.findOne({ user: req.user.id }).then(profile => {
     if (!profile) {
@@ -33,5 +33,64 @@ router.get("/userprofile", passport.authenticate('jwt', { session: false }), (re
     console.log(err);
   })
 });
+
+// @route GET api/profile
+// @desc Create or Edit User Profile
+// @access Private
+
+router.get("/", passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (req.body.handle) profileFields.handle = req.body.handle;
+  if (req.body.company) profileFields.company = req.body.company;
+  if (req.body.website) profileFields.website = req.body.website;
+  if (req.body.location) profileFields.location = req.body.location;
+  if (req.body.bio) profileFields.bio = req.body.bio;
+  if (req.body.status) profileFields.status = req.body.status;
+  if (req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+
+  //Skills - Split into Array
+  if (typeof req.body.skills !== 'undefined') {
+    profileFields.skills = req.body.skills.split(",");
+  }
+
+  // Social
+  profileFields.social = {};
+  if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+  if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+  if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+  if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if (profile) {
+        Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }).then(profile => res.json(profile));
+      }
+      else {
+        //Create
+
+        //Check if handle exists
+        Profile.findOne({ handle: profileFields.handle }).then(profile => {
+          if (profile) {
+            errors.handle = 'That handle already exists';
+            res.status(400).json(errors);
+          }
+
+          //Save Profile
+          new Profile(profileFields).save().then(profile => res.json(profile));
+        })
+      }
+    }).catch(err => { console.log(err); });
+
+
+});
+
+
 
 module.exports = router;
