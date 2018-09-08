cc5a04ab01c4047ed6d5e47e8eb2cc4ada13bf13
Lourdes Roashan <root.roashan@gmail.com>
2018-09-08 19:03:56 -0300

    Added initial auth redux-reducer

--- client/src/reducer/authReducer.js
+++ client/src/reducer/authReducer.js
@@ -1,3 +1,5 @@
+import { TEST_DISPATCH } from '../actions/types';
+
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -7,6 +9,11 @@ const initialState = {
 export default function (state = initialState, action) {
 
   switch (action.type) {
+    case TEST_DISPATCH:
+      return {
+        ...state,
+        user: action.payload
+      }
     default:
       return state;
   }
