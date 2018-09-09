6328dce2353be7c8fc8529292c12985523b75531
Lourdes Roashan <root.roashan@gmail.com>
2018-09-09 19:52:09 -0300

    Added Errors to Root Reducer

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import errorReducer from './errorReducer';
+
 
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  errors: errorReducer
 })
